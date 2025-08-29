82da69ffe701a3bda56b4ea3d497a5b293b17d5a
John Uchendu <johnchnd195@gmail.com>
2025-08-29 19:26:55 +0100

    next-sitemap changefreq to daily

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
     siteUrl: 'https://www.ibiz.name.ng',// replace wth your domain
     generateRobotsTxt: true, // (optional)
-    changefreq: 'weekly',
+    changefreq: 'daily',
     priority: 0.7,
     sitemapSize: 5000,
   };
