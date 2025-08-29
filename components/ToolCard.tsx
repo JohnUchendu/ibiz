0a6de8f82bfbcd729c55477990fb7454768da76a
John Uchendu <johnchnd195@gmail.com>
2025-08-29 09:52:40 +0100

    changed tool component link color to blue

--- components/ToolCard.tsx
+++ components/ToolCard.tsx
@@ -7,7 +7,7 @@ export default function ToolCard({
 }: { title: string; desc: string; href: string }) {
   return (
     <Link href={href} className="block rounded-lg border bg-white p-5 hover:shadow">
-      <h3 className="font-semibold">{title}</h3>
+      <h3 className="font-semibold text-blue-600 ">{title}</h3>
       <p className="mt-2 text-sm text-gray-600">{desc}</p>
     </Link>
   );
