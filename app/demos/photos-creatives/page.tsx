90369bf84fb5198b4eb38c57b4438a2300a576b7
John Uchendu <johnchnd195@gmail.com>
2025-10-16 16:00:46 +0100

    finance color edit

---
+++ app/demos/photos-creatives/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function PhotographyFrame() {
+  return (
+    <iframe
+      src="/demos/photos-creatives/temp/index.html"
+      className="w-full min-h-screen border-none"
+    />
+  );
+}
---
+++ app/demos/real-estate/temp/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function RealEstateFramer() {
+  return (
+    <iframe
+      src="/demos/real-estate/temp/index.html"
+      className="w-full min-h-screen border-none"
+    />
+  );
+}
